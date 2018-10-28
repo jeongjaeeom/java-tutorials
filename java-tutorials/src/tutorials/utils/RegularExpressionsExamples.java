25d5428d070bd1ddcedf78dffd9c5d1897812155
jeongjaeeom <jeongjae.eom@gmail.com>
2018-10-28 14:22:35 +0900

    Fix

--- java-tutorials/src/tutorials/utils/RegularExpressionsExamples.java
+++ java-tutorials/src/tutorials/utils/RegularExpressionsExamples.java
@@ -1,16 +1,12 @@
 package tutorials.utils;
 
-import static org.hamcrest.CoreMatchers.equalTo;
-import static org.hamcrest.CoreMatchers.is;
-import static org.junit.Assert.assertThat;
 import org.junit.Test;
 
 public class RegularExpressionsExamples {
 
   @Test
   public void test() {
-    System.out.println("Hello World!");
-    assertThat("1", is(equalTo("tt")));
+      System.out.println("Hello World!");
   }
   
   
