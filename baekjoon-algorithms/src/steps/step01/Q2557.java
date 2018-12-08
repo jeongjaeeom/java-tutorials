88c6787545c7ce67774ab305b0599a0349a4c825
jeongjaeeom <jeongjae.eom@gmail.com>
2018-12-08 12:49:38 +0900

    Fix up

--- baekjoon-algorithms/src/steps/step01/Q2557.java
+++
@@ -1,8 +0,0 @@
-package steps.step01;
-
-public class Q2557 {
-
-  public static void main(String[] args) {
-    System.out.println("Hello World!");
-  }
-}
--- hello-coding-algorithm/README.md
+++
@@ -1,1 +0,0 @@
-### Hello Coding 그림으로 개념을 이해하는 알고리즘 Java Examples 정리.
